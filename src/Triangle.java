cbf806a8d702ef5d89d4b1c5b5cb75c7b407b43f
Tulio Casagrande Alberto <tuliocasagrande@gmail.com>
2015-12-12 17:53:54 -0200

    Add easiest exercise ever

---
+++ src/Triangle.java
@@ -0,0 +1,13 @@
+/**
+ * Created by tulio on 12/12/15.
+ */
+public class Triangle {
+    public static void main(String[] args) {
+        easiestExerciseEver();
+    }
+
+    private static void easiestExerciseEver() {
+        System.out.println("*");
+    }
+
+}
