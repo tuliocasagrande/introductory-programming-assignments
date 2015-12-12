cfbad1f77d3c6aa7214015ae6fd8e9e544e7a72f
Tulio Casagrande Alberto <tuliocasagrande@gmail.com>
2015-12-12 18:03:26 -0200

    Finish triangle exercises

--- src/Triangle.java
+++ src/Triangle.java
@@ -4,6 +4,28 @@
 public class Triangle {
     public static void main(String[] args) {
         easiestExerciseEver();
+        drawHorizontalLine(8);
+        drawVerticalLine(3);
+        drawRightTriangle(3);
+    }
+
+    private static void drawRightTriangle(int size) {
+        for (int i = 0; i < size; i++) {
+            drawHorizontalLine(i+1);
+        }
+    }
+
+    private static void drawVerticalLine(int size) {
+        for (int i = 0; i < size; i++) {
+            drawHorizontalLine(1);
+        }
+    }
+
+    private static void drawHorizontalLine(int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 
     private static void easiestExerciseEver() {
