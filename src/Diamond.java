295fe0e76e47271c8c4467c4b3cca287336a00ce
Tulio Casagrande Alberto <tuliocasagrande@gmail.com>
2015-12-12 18:32:15 -0200

    Add isosceles triangle

---
+++ src/Diamond.java
@@ -0,0 +1,29 @@
+/**
+ * Created by tulio on 12/12/15.
+ */
+public class Diamond {
+
+    public static void main(String[] args) {
+        drawIsoscelesTriangle(3);
+        drawIsoscelesTriangle(4);
+    }
+
+    private static void drawIsoscelesTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            drawTriangleLine(i, size);
+        }
+    }
+
+    private static void drawTriangleLine(int line, int size) {
+        int asterisks = line*2 - 1;
+        int spaces = size - line;
+
+        for (int i = 0; i < spaces; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < asterisks; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
